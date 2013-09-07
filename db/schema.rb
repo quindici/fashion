7e5259bdd95787c7b4144eafda0b56d9aa4604d4
Quindici <britanniaman@gmail.com>
2013-09-07 09:26:42 +0100

    Added comment model to DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907075439) do
+ActiveRecord::Schema.define(version: 20130907082350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.integer  "score"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "look_photos", force: true do |t|
     t.string   "description"
