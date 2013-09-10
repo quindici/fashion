72c92a259c9668c99019538d4055197536891bd0
Quindici <britanniaman@gmail.com>
2013-09-10 13:25:04 +0100

    S3: Changing setting again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Hdil2::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :url => "s3-eu-west-1.amazonaws.com"
+    :s3_permissions => :private,
+    :s3_protocol => 'https',
+    :s3_host_name => "s3-eu-west-1.amazonaws.com"
   }
 end
